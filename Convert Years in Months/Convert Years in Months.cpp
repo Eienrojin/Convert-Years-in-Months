db32dc70457bc86b39412b3fffa2c1dec6fb8c7a
Nikita Polunin (二木立・半人) <Stkchanelx@gmail.com>
2021-10-19 15:57:36 +0300

    redacted

---
+++ Convert Years in Months/Convert Years in Months.cpp
@@ -0,0 +1,13 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double years, months;
+    cout << "Enter your age: " << endl;
+    cin >> years;
+
+    months = years * 12;
+    cout << "Your age in months is " << months << endl;
+}
